c6755ab60e0a72d5c366b567a4cd1025eedee6a9
Alexig2 <alex4342on@gmail.com>
2024-11-13 17:32:09 -0400

    Justes de conexão

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,28 +1,27 @@
-'use client'
+"use client";
 import { useState } from "react";
 import styles from "../page.module.css";
-import Usuario from "../interfaces/usuario"
-
+import styless from "../styles/cadastro.module.css";
+import Usuario from "../interfaces/usuario";
 import { FormEvent, useEffect } from "react";
 import { useRouter } from "next/navigation";
-
-import { setCookie, parseCookies } from 'nookies';
-import { ApiURL } from "../config";
+import { setCookie, parseCookies } from "nookies";
+import { ApiURL } from "../../../config";
 
 interface ResponseSignin {
-  erro: boolean,
-  mensagem: string,
-  token?: string
+  erro: boolean;
+  mensagem: string;
+  token?: string;
 }
 
 export default function Cadastro() {
   const [password, setPassword] = useState<string>();
   const [email, setEmail] = useState<string>();
   const [usuario, setUsuario] = useState<Usuario>({
-    nome: '',
-    email: '',
-    password: ''
-  })
+    nome: "",
+    email: "",
+    password: "",
+  });
 
   const [erro, setError] = useState("");
   const router = useRouter();
@@ -30,84 +29,106 @@ export default function Cadastro() {
   const alterarNome = (novoNome: string) => {
     setUsuario((valAntes) => ({
       ...valAntes,
-      nome: novoNome
-    }))
-  }
+      nome: novoNome,
+    }));
+  };
 
   const alterarEmail = (novoEmail: string) => {
     setUsuario((valoresAnteriores) => ({
       ...valoresAnteriores,
-      email: novoEmail
+      email: novoEmail,
     }));
   };
 
   const alterarSenha = (novaSenha: string) => {
     setUsuario((valoresAnteriores) => ({
       ...valoresAnteriores,
-      password: novaSenha
+      password: novaSenha,
     }));
   };
 
   useEffect(() => {
-    const { 'restaurant-token': token } = parseCookies();
+    const { "restaurant-token": token } = parseCookies();
     if (token) {
-      router.push('/');
+      router.push("/");
     }
   }, [router]);
 
-const  handleSubmit = async (e : FormEvent) => {
-  e.preventDefault();
-  try {
-   const response = await fetch(`${ApiURL}/auth/cadastro`, {
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({email, password})
-   })
-    if (response){
-      const data : ResponseSignin = await response.json()
-      const {erro, mensagem, token = ''} = data;
-      console.log(data)
-      if (erro){
-        setError(mensagem)
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${ApiURL}/auth/cadastro`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (response) {
+        const data: ResponseSignin = await response.json();
+        const { erro, mensagem, token = "" } = data;
+        console.log(data);
+        if (erro) {
+          setError(mensagem);
+        } else {
+          // npm i nookies setCookie
+          setCookie(undefined, "restaurant-token", token, {
+            maxAge: 60 * 60 * 1, // 1 hora
+          });
+        }
       } else {
-        // npm i nookies setCookie
-        setCookie(undefined, 'restaurant-token', token, {
-          maxAge: 60*60*1 // 1 hora
-        } )
-
       }
-    } else {
-
+    } catch (error) {
+      console.error("Erro na requisicao", error);
     }
-} 
- catch (error) {
-console.error('Erro na requisicao', error)
-}
-}
-
+  };
 
   return (
-    <main className={styles.page}>
-      <h1>Cadastro</h1>
-      <form onSubmit={handleSubmit}>
-      <div>
+    <main className={styless.fundo}>
+      <form onSubmit={handleSubmit} className={styless.container}>
+        <h1 className={styless.h1}>Cadastro</h1>
+        <div>
           <label>Nome</label>
-          <input type="text" name="nome" id="nome" value={usuario.nome} onChange={(e) => alterarNome(e.target.value)} required/>
+          <input
+            className={styless.input}
+            type="text"
+            name="nome"
+            id="nome"
+            value={usuario.nome}
+            onChange={(e) => alterarNome(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label>E-mail</label>
-          <input type="email" name="email" id="email" value={usuario.email} onChange={(e) => alterarEmail(e.target.value)} required/>
+          <input
+            className={styless.input}
+            type="email"
+            name="email"
+            id="email"
+            value={usuario.email}
+            onChange={(e) => alterarEmail(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label>Senha</label>
-          <input type="password" name="password" id="password" value={usuario.password} onChange={(e) => alterarSenha(e.target.value)} required/>
+          <input
+            className={styless.input}
+            type="password"
+            name="password"
+            id="password"
+            value={usuario.password}
+            onChange={(e) => alterarSenha(e.target.value)}
+            required
+          />
         </div>
 
         {erro && <p>{erro}</p>}
 
-        <button>Cadastrar</button>
+        <button type="submit" className={styless.button}>
+          Cadastrar
+        </button>
       </form>
     </main>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,77 +4,96 @@ import styless from "../styles/login.module.css";
 import { useRouter } from "next/navigation";
 import Usuario from "../interfaces/usuario";
 import { FormEvent, useEffect, useState } from "react";
-import { ApiURL } from "../config";
-import { setCookie , parseCookies} from "nookies";
+import { ApiURL } from "../../../config";
+import { setCookie, parseCookies } from "nookies";
 
 interface ResponseSignin {
-  erro: boolean,
-  mensagem: string,
-  token?: string
+  erro: boolean;
+  mensagem: string;
+  token?: string;
 }
 
-
-
 export default function Login() {
-  const router =useRouter()
+  const router = useRouter();
   const [password, setPassword] = useState<string>();
   const [email, setEmail] = useState<string>();
   const route = useRouter();
-  const [error, setError] = useState("")
-  
-  useEffect(() => {
-    const {'restaurant-token' : token} = parseCookies()
-    if (!token){
-      router.push('/login')
-    }
-  }, [])
+  const [error, setError] = useState("");
 
-  const  handleSubmit = async (e : FormEvent) => {
+  useEffect(() => {
+    const { "restaurant-token": token } = parseCookies();
+    if (!token) {
+      router.push("/login");
+    }
+  }, []);
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
-     const response = await fetch(`${ApiURL}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify({email, password})
-     })
-      if (response){
-        const data : ResponseSignin = await response.json()
-        const {erro, mensagem, token = ''} = data;
-        console.log(data)
-        if (erro){
-          setError(mensagem)
+      const response = await fetch(`${ApiURL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response) {
+
+        const data: ResponseSignin = await response.json();
+        const { erro, mensagem, token = "" } = data;
+
+        console.log(data);
+
+        if (erro) {
+          setError(mensagem);
         } else {
           // npm i nookies setCookie
-          setCookie(undefined, 'restaurant-token', token, {
-            maxAge: 60*60*1 // 1 hora
-          } )
-
+          setCookie(undefined, "restaurant-token", token, {
+            maxAge: 60 * 60 * 1, // 1 hora
+          });
         }
       } else {
-
       }
-  } 
-   catch (error) {
-  console.error('Erro na requisicao', error)
-}
-}
+    } catch (error) {
+      console.error("Erro na requisicao", error);
+    }
+    console.log("Email", email);
+    console.log("Senha", password);
+  };
 
   return (
     <main className={styless.fundo}>
       <form onSubmit={handleSubmit} className={styless.container}>
-      <h1 className={styless.h1}>Login</h1>
+        <h1 className={styless.h1}>Login</h1>
         <div>
           <label>E-mail</label>
-          <input className={styless.input} type="email" name="email" required onChange={(e) => {setEmail(e.target.value)}}/>
+          <input
+            className={styless.input}
+            type="email"
+            name="email"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label>Senha</label>
-          <input className={styless.input} type="password" name="senha" required onChange={(e) => {setPassword(e.target.value)}}/>
+          <input
+            className={styless.input}
+            type="password"
+            name="senha"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
 
-        <button type="submit" className={styless.button}>Entrar</button>
+        <button type="submit" className={styless.button}>
+          Entrar
+        </button>
 
         <p className={styless.a} onClick={() => route.push("/cadastro")}>
           Cadastrar
